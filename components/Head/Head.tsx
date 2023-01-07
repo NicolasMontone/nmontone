0523a01a11e6e1b9fc24325f1f23fe513a89bb43
NicolasMontone <montonenicolas01@gmail.com>
2023-01-07 16:54:47 -0300

    fix(Head): improve content and fix types.

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -2,21 +2,24 @@ import { default as NextHead } from 'next/head'
 
 import { Props } from './Head.types'
 
-const Head = ({ title, description, url }: Props): JSX.Element => (
+const Head = ({ title, description, path }: Props): JSX.Element => (
   <NextHead>
     <meta charSet="utf-8" />
     <title>{title}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content={description} />
     {/* Open graph */}
-    <meta property="og:site_name" content="Nicolás Montone" />
+    <meta
+      property="og:site_name"
+      content="Nicolás Montone - Fullstack dev, solving problems with technology"
+    />
     {/* this check is to not get undefined values */}
     {title && <meta property="og:title" content={title} />}
     <meta property="og:description" content={description} />
 
     <meta
       property="og:url"
-      content={`https://nicolasmontone.com/${url || ''}`}
+      content={`https://nicolasmontone.com/${path || ''}`}
     />
 
     {/* end of social media meta tags */}
