8d9ecb6351931b24e2be212421d61d8b5c505f51
NicolasMontone <montonenicolas01@gmail.com>
2023-01-08 11:53:44 -0300

    fix(Navbar): add logo.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { locations } from '../../lib/constants/locations'
 import { GithubIcon, TwitterIcon } from '../Icon'
@@ -10,30 +12,42 @@ const Navbar = () => {
   return (
     <Section className={styles.Navbar}>
       <nav className={styles.container}>
-        <NavbarItem content={'Blog'} title={'Blog'} href={locations.blog()} />
-        <NavbarItem
-          content={'Projects'}
-          title={'Projects'}
-          href={locations.projects()}
-        />
-        <NavbarItem
-          content={
-            <a target={'_blank'} rel="noopener noreferrer">
-              <GithubIcon />
-            </a>
-          }
-          title={'Github'}
-          href={'https://github.com/NicolasMontone'}
-        />
-        <NavbarItem
-          content={
-            <a target={'_blank'} rel="noopener noreferrer">
-              <TwitterIcon />
-            </a>
-          }
-          title={'Twitter'}
-          href={'https://twitter.com/montonenico'}
-        />
+        <div className={styles.navbarIcon}>
+          <Link href={locations.home()}>
+            <Image
+              width={40}
+              height={40}
+              src={'/icon.svg'}
+              alt={'Nicolás Montone Icon'}
+            />
+          </Link>
+        </div>
+        <div className={styles.items}>
+          <NavbarItem content={'Blog'} title={'Blog'} href={locations.blog()} />
+          <NavbarItem
+            content={'Projects'}
+            title={'Projects'}
+            href={locations.projects()}
+          />
+          <NavbarItem
+            content={
+              <a target={'_blank'} rel="noopener noreferrer">
+                <GithubIcon />
+              </a>
+            }
+            title={'Github'}
+            href={'https://github.com/NicolasMontone'}
+          />
+          <NavbarItem
+            content={
+              <a target={'_blank'} rel="noopener noreferrer">
+                <TwitterIcon />
+              </a>
+            }
+            title={'Twitter'}
+            href={'https://twitter.com/montonenico'}
+          />
+        </div>
       </nav>
     </Section>
   )
