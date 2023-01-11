01adbe7f74e05f6f6fdf26d9863b8f360171d35e
NicolasMontone <montonenicolas01@gmail.com>
2023-01-11 18:32:54 -0300

    fix(NavbarItem): remove unneeded log 🤦.

--- components/Navbar/NavbarItem/NavbarItem.tsx
+++ components/Navbar/NavbarItem/NavbarItem.tsx
@@ -2,17 +2,12 @@ import Link from 'next/link'
 import React from 'react'
 import { Props } from './NavbarItem.types'
 
-const NavbarItem = ({ content, href, title, className }: Props) => {
-  const isExternalUrl = href.startsWith('https://')
-  console.log('isExternalUrl', isExternalUrl)
-
-  return (
-    <div className={className || ''} title={title}>
-      <Link href={href} passHref>
-        {content}
-      </Link>
-    </div>
-  )
-}
+const NavbarItem = ({ content, href, title, className }: Props) => (
+  <div className={className || ''} title={title}>
+    <Link href={href} passHref>
+      {content}
+    </Link>
+  </div>
+)
 
 export default NavbarItem
