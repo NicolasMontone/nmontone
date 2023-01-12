e11f7b9edf5145e5c40ddf5ab642b8ac194c85c0
NicolasMontone <montonenicolas01@gmail.com>
2023-01-11 21:25:27 -0300

    fix(Navbar): remove unneeded braces.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,61 +5,60 @@ import { locations } from '../../lib/constants/locations'
 import { GithubIcon, TwitterIcon, VscoIcon } from '../Icon'
 import { Section } from '../Section'
 
-import styles from './Navbar.module.css'
 import { NavbarItem } from './NavbarItem'
 
-const Navbar = () => {
-  return (
-    <Section className={styles.Navbar} fixedWidth={false}>
-      <nav className={styles.container}>
-        <div className={styles.navbarIcon}>
-          <Link href={locations.home()}>
-            <Image
-              width={40}
-              height={40}
-              src={'/icon.svg'}
-              alt={'Nicolás Montone Icon'}
-            />
-          </Link>
-        </div>
-        <div className={styles.items}>
-          <NavbarItem content={'Blog'} title={'Blog'} href={locations.blog()} />
-          <NavbarItem
-            content={'Projects'}
-            title={'Projects'}
-            href={locations.projects()}
+import styles from './Navbar.module.css'
+
+const Navbar = () => (
+  <Section className={styles.Navbar} fixedWidth={false}>
+    <nav className={styles.container}>
+      <div className={styles.navbarIcon}>
+        <Link href={locations.home()}>
+          <Image
+            width={40}
+            height={40}
+            src={'/icon.svg'}
+            alt={'Nicolás Montone Icon'}
           />
-          <NavbarItem
-            content={
-              <a target={'_blank'} rel="noopener noreferrer">
-                <GithubIcon />
-              </a>
-            }
-            title={'Github'}
-            href={'https://github.com/NicolasMontone'}
-          />
-          <NavbarItem
-            content={
-              <a target={'_blank'} rel="noopener noreferrer">
-                <TwitterIcon />
-              </a>
-            }
-            title={'Twitter'}
-            href={'https://twitter.com/montonenico'}
-          />
-          <NavbarItem
-            content={
-              <a target={'_blank'} rel="noopener noreferrer">
-                <VscoIcon />
-              </a>
-            }
-            title={'Vsco'}
-            href={'https://vsco.co/nicomontone/gallery'}
-          />
-        </div>
-      </nav>
-    </Section>
-  )
-}
+        </Link>
+      </div>
+      <div className={styles.items}>
+        <NavbarItem content={'Blog'} title={'Blog'} href={locations.blog()} />
+        <NavbarItem
+          content={'Projects'}
+          title={'Projects'}
+          href={locations.projects()}
+        />
+        <NavbarItem
+          content={
+            <a target={'_blank'} rel="noopener noreferrer">
+              <GithubIcon />
+            </a>
+          }
+          title={'Github'}
+          href={'https://github.com/NicolasMontone'}
+        />
+        <NavbarItem
+          content={
+            <a target={'_blank'} rel="noopener noreferrer">
+              <TwitterIcon />
+            </a>
+          }
+          title={'Twitter'}
+          href={'https://twitter.com/montonenico'}
+        />
+        <NavbarItem
+          content={
+            <a target={'_blank'} rel="noopener noreferrer">
+              <VscoIcon />
+            </a>
+          }
+          title={'Vsco'}
+          href={'https://vsco.co/nicomontone/gallery'}
+        />
+      </div>
+    </nav>
+  </Section>
+)
 
 export default Navbar
