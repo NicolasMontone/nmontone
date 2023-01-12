44372f65c8ee9224b37ff9c8e6ecc0f5c446ef3a
NicolasMontone <montonenicolas01@gmail.com>
2023-01-12 00:25:22 -0300

    feat: add ga script.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -9,6 +10,19 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&family=Noto+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
+        />
+
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-GPYT1RP7ZN"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+                     function gtag(){dataLayer.push(arguments);}
+                     gtag('js', new Date());
+                     gtag('config', 'G-GPYT1RP7ZN');`,
+          }}
         />
       </Head>
 
