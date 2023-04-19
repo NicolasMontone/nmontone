0810fe92967d4b9779fe652d71848a46b7abd506
NicolasMontone <montonenicolas01@gmail.com>
2023-04-18 21:37:29 -0300

    feat: add next last version and fix components.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,8 +5,6 @@ import { locations } from '../../lib/constants/locations'
 import { GithubIcon, TwitterIcon, VscoIcon } from '../Icon'
 import { Section } from '../Section'
 
-import { NavbarItem } from './NavbarItem'
-
 import styles from './Navbar.module.css'
 
 const Navbar = () => (
@@ -23,39 +21,32 @@ const Navbar = () => (
         </Link>
       </div>
       <div className={styles.items}>
-        <NavbarItem content={'Blog'} title={'Blog'} href={locations.blog()} />
-        <NavbarItem
-          content={'Projects'}
-          title={'Projects'}
-          href={locations.projects()}
-        />
-        <NavbarItem
-          content={
-            <a target={'_blank'} rel="noopener noreferrer">
-              <GithubIcon />
-            </a>
-          }
-          title={'Github'}
+        <Link href={locations.blog()}>Blog</Link>
+
+        <Link href={locations.projects()}>Projects</Link>
+        <a
+          target={'_blank'}
+          rel="noopener noreferrer"
           href={'https://github.com/NicolasMontone'}
-        />
-        <NavbarItem
-          content={
-            <a target={'_blank'} rel="noopener noreferrer">
-              <TwitterIcon />
-            </a>
-          }
-          title={'Twitter'}
+        >
+          <GithubIcon />
+        </a>
+
+        <a
+          target={'_blank'}
+          rel="noopener noreferrer"
           href={'https://twitter.com/montonenico'}
-        />
-        <NavbarItem
-          content={
-            <a target={'_blank'} rel="noopener noreferrer">
-              <VscoIcon />
-            </a>
-          }
-          title={'Vsco'}
+        >
+          <TwitterIcon />
+        </a>
+
+        <a
+          target={'_blank'}
+          rel="noopener noreferrer"
           href={'https://vsco.co/nicomontone/gallery'}
-        />
+        >
+          <VscoIcon />
+        </a>
       </div>
     </nav>
   </Section>
